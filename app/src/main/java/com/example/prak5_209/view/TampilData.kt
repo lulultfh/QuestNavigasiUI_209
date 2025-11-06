82a775a24ed7624bb89d765d085cc3058a2713d3
lululthfh <punyalulu05@gmail.com>
2025-11-06 13:05:10 +0700

    feat: menambahkan Composable function bernamakan FormIsian dengan parameter jenisK untuk menampilkan list Laki-laki dan Perempuan, dan OnSubmitBtnClick sebagai function yang dijalankan ketika Submit di klik

--- app/src/main/java/com/example/prak5_209/view/TampilData.kt
+++ app/src/main/java/com/example/prak5_209/view/TampilData.kt
@@ -1,2 +1,9 @@
 package com.example.prak5_209.view
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){}
