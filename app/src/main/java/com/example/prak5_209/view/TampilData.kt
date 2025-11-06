bdaeaa8ec9d4fdcaac0d01cb1e786b8eb61ce3bb
lululthfh <punyalulu05@gmail.com>
2025-11-06 14:12:46 +0700

    feat: menambahkan Composable function TampilData yang memiliki parameter OnBackBtnClick

--- app/src/main/java/com/example/prak5_209/view/TampilData.kt
+++ app/src/main/java/com/example/prak5_209/view/TampilData.kt
@@ -3,5 +3,10 @@
 package com.example.prak5_209.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBackBtnClick: ()-> Unit
+){}
