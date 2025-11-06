544c9b7a98b8d65e27da9fa60ef70e587a56f693
lululthfh <punyalulu05@gmail.com>
2025-11-06 13:20:02 +0700

    refactor: delete function FormIsian dalam TampilData.kt

--- app/src/main/java/com/example/prak5_209/view/TampilData.kt
+++ app/src/main/java/com/example/prak5_209/view/TampilData.kt
@@ -1,9 +1,1 @@
 package com.example.prak5_209.view
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun FormIsian(
-    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
-    OnSubmitBtnClick : () -> Unit
-){}
