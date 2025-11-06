9d479372d9ec78f9bc8a0741ce28e8449a0f5467
lululthfh <punyalulu05@gmail.com>
2025-11-06 14:29:30 +0700

    feat: menambahkan layout COlumn dalam layout Column pada body Scaffold dalam function TampilData dengan parameter modifier padding yang diambil dari dimensionResource dengan id padding_small dan verticalArangement spacedBy yang diambil dari dimensionResource dengan id padding_small

--- app/src/main/java/com/example/prak5_209/view/TampilData.kt
+++ app/src/main/java/com/example/prak5_209/view/TampilData.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import com.example.prak5_209.R
 
@@ -35,6 +36,9 @@ fun TampilData(
             )
         }) { isiRuang ->
         Column(modifier = Modifier.padding(paddingValues = isiRuang),
-            verticalArrangement = Arrangement.SpaceBetween) {  }
+            verticalArrangement = Arrangement.SpaceBetween) {
+            Column(modifier = Modifier.padding(all = dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(space = dimensionResource(id = R.dimen.padding_small))) {  }
+        }
     }
 }
