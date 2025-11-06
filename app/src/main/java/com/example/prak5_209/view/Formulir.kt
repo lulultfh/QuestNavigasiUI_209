15b62601617841cebae636337c04fe28c6b2a79d
lululthfh <punyalulu05@gmail.com>
2025-11-06 13:46:03 +0700

    feat: menambahkkan komponen OutlinedTextField dengan label Nama Lengkap

--- app/src/main/java/com/example/prak5_209/view/Formulir.kt
+++ app/src/main/java/com/example/prak5_209/view/Formulir.kt
@@ -5,7 +5,9 @@ package com.example.prak5_209.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -16,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.prak5_209.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -37,6 +40,16 @@ fun FormIsian(
         }) { isiRuang->
         Column(modifier = Modifier.padding(paddingValues = isiRuang),
             verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally) {  }
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = {Text(text = "Nama Lengkap")}
+            )
+        }
     }
 }
