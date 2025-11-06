07c0c2f30cc7f315f7115edbf0cf5afa339e0090
lululthfh <punyalulu05@gmail.com>
2025-11-06 13:58:34 +0700

    feat: menambahkan layout Row yang menampilkan setiap item pada jenisK dengan loop forEach, yang didalamnya terdapat layout Row yang berisikan komponen RadioButton dan komponen Text

--- app/src/main/java/com/example/prak5_209/view/Formulir.kt
+++ app/src/main/java/com/example/prak5_209/view/Formulir.kt
@@ -4,12 +4,14 @@ package com.example.prak5_209.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -56,6 +58,18 @@ fun FormIsian(
                 .padding(all = 20.dp)
                 .width(width = 250.dp), thickness = Thickness,
                 color = Color.Red)
+            Row {
+                jenisK.forEach {
+                    item ->
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        RadioButton(
+                            selected = false,
+                            onClick = {item}
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
         }
     }
 }
