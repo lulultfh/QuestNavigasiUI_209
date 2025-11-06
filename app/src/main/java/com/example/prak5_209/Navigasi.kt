b7d9b4f10ddd229b5343e0c79fc1ab195a2d4c85
lululthfh <punyalulu05@gmail.com>
2025-11-06 09:16:18 +0700

    feat: menambahkan Composable function bernamakan DataApp dengan parameter NavController untuk mengatur navigasi dan modifier

--- app/src/main/java/com/example/prak5_209/Navigasi.kt
+++ app/src/main/java/com/example/prak5_209/Navigasi.kt
@@ -1,6 +1,17 @@
 package com.example.prak5_209
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi{
     Formulir,
     Detail
 }
+
+@Composable
+fun DataApp(
+    navController: NavController = rememberNavController(),
+    modifier: Modifier
+){}
