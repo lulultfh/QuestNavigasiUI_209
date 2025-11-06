78616141e3001c118abceae98f7f9361872c057a
lululthfh <punyalulu05@gmail.com>
2025-11-06 09:27:37 +0700

    feat: menambahkan Scaffold sebagai main layout dengan NavHost yang memiliki parameter untuk mendefinisikan halaman pertama, navController, dan padding isiRuang

--- app/src/main/java/com/example/prak5_209/Navigasi.kt
+++ app/src/main/java/com/example/prak5_209/Navigasi.kt
@@ -1,11 +1,13 @@
 package com.example.prak5_209
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi{
@@ -18,4 +20,14 @@ fun DataApp(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier
 ){
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ){
+
+        }
+    }
 }
